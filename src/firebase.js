a9720ca8060caf99f2539dbae25262b4b3d24e0e
ssinking91 <tlsgkdals71@naver.com>
2021-12-12 04:06:26 +0900

    firestore인증키

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+    //firestore 인증키
 };
 
 initializeApp(firebaseConfig);
