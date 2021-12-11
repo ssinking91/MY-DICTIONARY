5a60de153efd2b2911639b1501868740cd5a61d3
ssinking91 <tlsgkdals71@naver.com>
2021-12-12 04:04:03 +0900

    My_dictionary

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+// 리액트 패키지를 불러옵니다.
+import React from "react";
+
+const Test = (props) => {
+    
+    return (
+        <div className="test" >
+            <h1>테스트 입니다.</h1>
+        </div>
+    );
+};
+
+
+export default Test;
